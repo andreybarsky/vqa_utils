02d316d62b747b72f21de6630942dbfe86c35fc6
Andrey Barsky <andreybarsky@gmail.com>
2024-05-03 15:05:04 +0200

    extra paths

--- filepaths.py
+++ filepaths.py
@@ -8,7 +8,12 @@
 ### v1.0 (the large set):
 # imdb_dir = '/home/abarsky/data/PFLDocVQA_centralized/imdb'
 ### v1.1 (the 300k set):
-imdb_dir = '/home/abarsky/data/elsa/data_v1.0/blue'
+# imdb_dir = '/home/abarsky/data/elsa/data_v1.0/blue'
+### v1.2 (the large set, reshuffled):
+imdb_dir = '/home/abarsky/data/DocVQA_1.2/imdb'
 
-images_dir = '/home/abarsky/data/PFLDocVQA_centralized/images'
+# images, and the h5 file for those images, are independent of dataset version:
+# images_dir = '/home/abarsky/data/PFLDocVQA_centralized/images'
+images_dir = '/home/abarsky/data/PFL-DocVQA/images/'
 h5_path = '/home/abarsky/data/PFLDocVQA_h5/blue.h5'
+kv_path = '/home/abarsky/data/elsa/build/total_kv_textract.json'
