5e0a3fd8735f566e4f318f3445712e77adc4d15b
Andrey Barsky <andreybarsky@gmail.com>
2024-06-04 16:58:12 +0200

    cleanup

--- filepaths.py
+++ filepaths.py
@@ -1,24 +1,6 @@
-#### for pfl-docvqa:
-#
-# imdb_dir = '/home/abarsky/data/PFL-DocVQA/imdb'
-# images_dir = '/home/abarsky/data/PFL-DocVQA/images'
 
-## centralised version:
-
-### v1.0 (the large set):
-# old_imdb_dir = '/home/abarsky/data/PFLDocVQA_centralized/imdb'
-old_imdb_dir = '/home/abarsky/data/PFL-DocVQA/imdb'
-### v1.1 (the 300k set):
-# imdb_dir = '/home/abarsky/data/elsa/data_v1.0/blue'
-### v1.2 (the large set, reshuffled):
-imdb_dir = '/home/abarsky/data/PFL-DocVQA/imdb/1.3a/'
-
-
-# images, and the h5 file for those images, are independent of dataset version:
-# images_dir = '/home/abarsky/data/PFLDocVQA_centralized/images'
+# directory of image files (we expect jpgs)
 images_dir = '/home/abarsky/data/PFL-DocVQA/images/full/'
-h5_path = '/home/abarsky/data/PFL-DocVQA/images/blue.h5'
 
-# misc datafiles and mappings:
-# kv_path = '/home/abarsky/data/elsa/build/total_kv_textract.json'
-# provider_path = '/home/abarsky/data/elsa/provider2docs.json'
+# directory of imdb .npy files:
+imdb_dir = '/home/abarsky/data/PFL-DocVQA/imdb/1.3a/'
